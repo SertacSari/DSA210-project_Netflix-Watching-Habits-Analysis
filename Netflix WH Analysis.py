6e0de3c1a4f47780db19064ab4d9f5065f908507
SertacSari <ahmetssari53@gmail.com>
2025-01-09 22:42:10 +0300

    converting date-time +

--- Netflix WH Analysis.py
+++ Netflix WH Analysis.py
@@ -18,3 +18,19 @@ df = pd.read_csv(
         "Country"
     ]
 )
+
+# Filter the Data for Profile Name: "Sertaç"
+df = df[df["ProfileName"] == "Sertaç"]
+
+# Convert 'StartTime' to a proper datetime format
+df["StartTime"] = pd.to_datetime(
+    df["StartTime"], 
+    format="%Y-%m-%d %H:%M:%S",  
+    errors="coerce"  # Turn invalid parsing into NaT
+)
+
+# Extract only the Date (yyyy-mm-dd) from 'StartTime'
+df["Date"] = df["StartTime"].dt.date
+
+# Convert 'Duration' (HH:MM:SS) to hours
+df["Duration"] = pd.to_timedelta(df["Duration"], errors="coerce").dt.total_seconds() / 3600
